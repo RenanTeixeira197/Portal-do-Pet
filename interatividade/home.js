36736cbe57923a608b6ccf8476a597fee6ff5cc1
Renan Teixeira <rteixeira1997@gmail.com>
2025-09-14 00:43:38 -0300

    update

--- interatividade/home.js
+++ interatividade/home.js
@@ -15,13 +15,15 @@ const nextButton = document.querySelector('.next');
 
 let index = 0;
 
+// ...existing code...
 function showSlide(n) {
     index = (n + images.length) % images.length;
-    track.style.transform = `translateX(${index * 100}%)`;
+    track.style.transform = `translateX(${-index * 100}%)`;
 }
 
 prevButton.addEventListener('click', () => showSlide(index - 1));
 nextButton.addEventListener('click', () => showSlide(index + 1));
 
 //Troca automaticamente a cada 5 segundos
-//setInterval(() => showSlide(index +1), 500);
+setInterval(() => showSlide(index + 1), 5000);
+// ...existing code...
